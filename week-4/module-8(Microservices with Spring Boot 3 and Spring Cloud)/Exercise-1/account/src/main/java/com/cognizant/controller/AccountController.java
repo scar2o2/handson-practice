de386dbebb5376a3913070d3cc4261b67ddc3e33
scar2o2 <chinnasmart202@gmail.com>
2026-07-25 10:21:01 +0530

    completed week-5,6

---
+++ week-4/module-8(Microservices with Spring Boot 3 and Spring Cloud)/Exercise-1/account/src/main/java/com/cognizant/controller/AccountController.java
@@ -0,0 +1,19 @@
+package com.cognizant.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.model.Account;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/accounts/{number}")
+    public Account getAccount(@PathVariable String number) {
+
+        return new Account(number, "Savings", 234343);
+
+    }
+
+}
---
+++ week-4/module-8(Microservices with Spring Boot 3 and Spring Cloud)/Exercise-1/loan/src/main/java/com/cognizant/controller/LoanController.java
@@ -0,0 +1,24 @@
+package com.cognizant.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.model.Loan;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loans/{number}")
+    public Loan getLoan(@PathVariable String number) {
+
+        return new Loan(
+                number,
+                "Car",
+                400000,
+                3258,
+                18);
+
+    }
+
+}
